679e65bd0e47f2151fa7fc9612ac7602dddf9a14
KAMIYA HIDEKI <hidekikamiyajpn@KAMIYA-no-MacBook-Air.local>
2016-12-08 15:07:23 +0900

    レイアウトを作成する1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "static/Pages"
+  get "static/home"
+  get "static/help"
+  get "static/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
